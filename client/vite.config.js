bdea11e2ba8e91387feca7c3f15916acda39e75a
Rahul7_77 <rahuldasari7502@gmail.com>
2024-07-09 19:02:46 +0530

    Updated frontend code and versions

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react' //removed swc
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
 })
