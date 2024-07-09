3e335c0970e3fbf686f31c387b17a0b21bf92db1
vikas gupta <vikasguptaamit.com>
2024-07-09 19:21:15 +0530

    updated fronted

--- client/src/components/ChatPage.jsx
+++ client/src/components/ChatPage.jsx
@@ -1,69 +1,9 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import './ChatPage.css'
-
-// function ChatPage() {
-//   const [messages, setMessages] = useState([]);
-//   const [userMessage, setUserMessage] = useState('');
-//   const [pdfText, setPdfText] = useState('');
-
-//   useEffect(() => {
-//     const storedText = localStorage.getItem('pdfText');
-//     if (storedText) {
-//       setPdfText(storedText);
-//     }
-//   }, []);
-
-//   const handleMessageSend = async () => {
-//     if (!userMessage.trim()) return;
-
-//     const newMessage = { user: 'User', text: userMessage };
-//     setMessages([...messages, newMessage]);
-
-//     try {
-//       const response = await axios.post('/chat', { message: userMessage });
-//       const botMessage = { user: 'Bot', text: response.data.response };
-//       setMessages((prevMessages) => [...prevMessages, botMessage]);
-//     } catch (error) {
-//       console.error('Error sending message:', error);
-//     }
-
-//     setUserMessage('');
-//   };
-
-//   return (
-//     <div className="ChatPage">
-//       <h1>Chat with Bot</h1>
-//       <div id="chatBox">
-//         <div id="messages">
-//           {messages.map((msg, index) => (
-//             <div key={index} className={`message ${msg.user}`}>
-//               <strong>{msg.user}:</strong> {msg.text}
-//             </div>
-//           ))}
-//         </div>
-//         <input
-//           type="text"
-//           value={userMessage}
-//           onChange={(e) => setUserMessage(e.target.value)}
-//         />
-//         <button onClick={handleMessageSend}>Send</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ChatPage;
-
-
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import './ChatPage.css';
 
 function ChatPage() {
-  const location = useLocation();
-  const { pdfText } = location.state || {};
   const projectID = localStorage.getItem('projectID');
   const [messages, setMessages] = useState([]);
 
