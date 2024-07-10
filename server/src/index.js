7f1943cab221fb05703b01a32e3c7b363c060b9f
Rahul7_77 <rahuldasari7502@gmail.com>
2024-07-10 12:22:50 +0530

    Added docker images code and k8s code

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use('/', projectRouter);
 
